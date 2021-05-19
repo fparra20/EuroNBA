dc126393b2db6aef956af5469ff5174d56ce0384
Parra <fpm015@gmail.com>
2021-05-19 18:58:12 +0200

    DB implemented

--- app/src/main/java/com/example/PlayerActivity.java
+++ app/src/main/java/com/example/PlayerActivity.java
@@ -2,15 +2,75 @@ package com.example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.widget.ImageView;
 
+import com.example.controller.TeamsDatabaseHelper;
 import com.example.euronba.R;
+import com.example.euronba.model.Team;
+
+import java.time.Clock;
+import java.util.ArrayList;
 
 public class PlayerActivity extends AppCompatActivity {
 
+    ArrayList<Team> teamsList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
+
+        // Creamos el cursor para traer los datos de la BD
+        SQLiteOpenHelper TeamsDatabaseHelper = new TeamsDatabaseHelper(getApplicationContext());
+
+        // Extrae la base de datos para trabajar con ella
+        SQLiteDatabase db = TeamsDatabaseHelper.getReadableDatabase();
+
+        // Crea una consulta en la tabla TEAMS con todas sus columnas
+        // Además, las ordena por nombre de ciudad alfabéticamente
+        Cursor cursor = db.query("TEAMS",
+                new String[]{"IMAGE_LOGO", "TEAM_ID", "CITY", "NICKNAME", "FULLNAME", "TRICODE", "CONFNAME", "DIVNAME", "URLNAME"}, null,
+                null, null, null, "CITY");
+
+        //Creamos un arraylist de tipo Pet vacío
+        teamsList = new ArrayList<Team>();
+
+        // Comprobamos que el cursor no está vacío
+        if (cursor.moveToFirst()) {
+
+            // Este código se repite mientras cursor siga teniendo datos
+            do {
+                // Para cada dato del cursor introducimos un nuevo valor en el ArrayList
+                teamsList.add(new Team(
+                        cursor.getInt(0),
+                        cursor.getString(1),
+                        cursor.getString(2),
+                        cursor.getString(3),
+                        cursor.getString(4),
+                        cursor.getString(5),
+                        cursor.getString(6),
+                        cursor.getString(7),
+                        cursor.getString(8)));
+
+            } while (cursor.moveToNext());
+
+            // Si no tiene datos o no le quedan, cierra el cursor y las llamadas a la base de datos.
+        } else {
+            cursor.close();
+            db.close();
+        }
+
+        ImageView iv = findViewById(R.id.iv);
+
+        iv.setImageResource(teamsList.get(29).getLogo());
+        System.out.println("Logo: "+teamsList.get(0).getLogo());
+        System.out.println("ID: "+teamsList.get(0).getTeamId());
+        System.out.println("Full Name: "+teamsList.get(0).getFullName());
+        System.out.println("Conf: "+teamsList.get(0).getConfName());
+        System.out.println("Div: "+teamsList.get(0).getDivName());
+        System.out.println("URL: "+teamsList.get(0).getUrlName());
     }
 }
--- app/src/main/java/com/example/controller/TeamsDatabaseHelper.java
+++ app/src/main/java/com/example/controller/TeamsDatabaseHelper.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.example.euronba.R;
+import com.example.euronba.model.Team;
 
 //Clase auxiliar para el tratamiento de la base de datos
 public class TeamsDatabaseHelper extends SQLiteOpenHelper {
@@ -44,26 +45,55 @@ public class TeamsDatabaseHelper extends SQLiteOpenHelper {
          * posteriormente a su creación se actualiza (oldVersion<2)
          */
         if (oldVersion < 1) {
-            db.execSQL("CREATE TABLE PET (_id INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + "IMAGE_RESOURCE_ID INTEGER, "
-                    + "NAME TEXT, "
-                    + "RATING INTEGER);");
+            db.execSQL("CREATE TABLE TEAMS (IMAGE_LOGO INTEGER,"
+                    + "TEAM_ID TEXT PRIMARY KEY,"
+                    + "CITY TEXT,"
+                    + "NICKNAME TEXT,"
+                    + "FULLNAME TEXT,"
+                    + "TRICODE TEXT,"
+                    + "CONFNAME TEXT,"
+                    + "DIVNAME TEXT,"
+                    + "URLNAME TEXT);");
             //El metodo auxiliar insertPet lo creamos para insertar varios filas
-            /*
-            insertPet(db, R.drawable.camaleon, "Chamaleon", 0);
-            insertPet(db, R.drawable.mountain_horned_dragon, "Mountain Horned Dragon", 0);
-            insertPet(db, R.drawable.russian_turtle, "Russian turtle", 0);
-            insertPet(db, R.drawable.fire_skink, "Fire Skink", 0);
-            insertPet(db, R.drawable.red_eared_slider_turtle, "Red Eared Turtle", 0);
-            insertPet(db, R.drawable.baby_bearded_dragon, "Baby Bearded Dragon", 0);
-            insertPet(db, R.drawable.gecko, "Common Gecko", 0);
-            insertPet(db, R.drawable.ball_phyton, "Ball Phyton", 0);
-            insertPet(db, R.drawable.anolis_carolinensis, "Anolis Carolinensis", 0);
-            insertPet(db, R.drawable.leopard_gecko, "Leopard Gecko", 0);
-             */
+            insertTeam(db, R.drawable.bos, "1610612738", "Boston", "Celtics", "Boston Celtics", "BOS", "East", "Atlantic", "celtics");
+            insertTeam(db, R.drawable.brk, "1610612751", "Brooklyin", "Nets", "Brooklyin Nets", "BKN", "East", "Atlantic", "nets");
+            insertTeam(db, R.drawable.phi, "1610612755", "Philadelphia", "76ers", "Philadelphia 76ers", "PHI", "East", "Atlantic", "sixers");
+            insertTeam(db, R.drawable.nyk, "1610612752", "New York", "Knicks", "New York Knicks", "NYK", "East", "Atlantic", "knicks");
+            insertTeam(db, R.drawable.tor, "1610612761", "Toronto", "Raptors", "Toronto Raptors", "TOR", "East", "Atlantic", "raptors");
+
+            insertTeam(db, R.drawable.atl, "1610612737", "Atlanta", "Hawks", "Atlanta Hawks", "ATL", "East", "Southeast", "hawks");
+            insertTeam(db, R.drawable.cho, "1610612766", "Charlotte", "Hornets", "Charlotte Hornets", "CHA", "East", "Southeast", "hornets");
+            insertTeam(db, R.drawable.orl, "1610612753", "Orlando", "Magic", "Orlando Magic", "ORL", "East", "Southeast", "magic");
+            insertTeam(db, R.drawable.mia, "1610612748", "Miami", "Heat", "Miami Heat", "MIA", "East", "Southeast", "heat");
+            insertTeam(db, R.drawable.was, "1610612764", "Washington", "Wizards", "Washington Wizards", "WAS", "East", "Southeast", "wizards");
+
+            insertTeam(db, R.drawable.chi, "1610612741", "Chicago", "Bulls", "Chicago Bulls", "CHI", "East", "Central", "bulls");
+            insertTeam(db, R.drawable.cle, "1610612739", "Cleveland", "Cavaliers", "Cleveland Cavaliers", "CLE", "East", "Central", "cavaliers");
+            insertTeam(db, R.drawable.det, "1610612765", "Detroit", "Pistons", "Detroit Pistons", "DET", "East", "Central", "pistons");
+            insertTeam(db, R.drawable.ind, "1610612754", "Indiana", "Pacers", "Indiana Pacers", "IND", "East", "Central", "pacers");
+            insertTeam(db, R.drawable.mil, "1610612749", "Milwaukee", "Bucks", "Indiana Pacers", "IND", "East", "Central", "bucks");
+
+            insertTeam(db, R.drawable.dal, "1610612742", "Dallas", "Mavericks", "Dallas Mavericks", "DAL", "West", "Southwest", "mavericks");
+            insertTeam(db, R.drawable.hou, "1610612745", "Houston", "Rockets", "Houston Rockets", "HOU", "West", "Southwest", "rockets");
+            insertTeam(db, R.drawable.mem, "1610612763", "Memphis", "Grizzlies", "Memphis Grizzlies", "MEM", "West", "Southwest", "grizzlies");
+            insertTeam(db, R.drawable.sas, "1610612759", "San Antonio", "Spurs", "San Antonio Spurs", "SAS", "West", "Southwest", "spurs");
+            insertTeam(db, R.drawable.nop, "1610612740", "New Orleans", "Pelicans", "New Orleans Pelicans", "NOP", "West", "Southwest", "pelicans");
+
+            insertTeam(db, R.drawable.den, "1610612743", "Denver", "Nuggets", "Denver Nuggets", "DEN", "West", "Northwest", "nuggets");
+            insertTeam(db, R.drawable.min, "1610612750", "Minnesota", "Timberwolves", "Minnesota Timberwolves", "MIN", "West", "Northwest", "timberwolves");
+            insertTeam(db, R.drawable.okc, "1610612760", "Oklahoma City", "Thunder", "Oklahoma City Thunder", "OKC", "West", "Northwest", "thunder");
+            insertTeam(db, R.drawable.por, "1610612757", "Portland", "Trail Blazers", "Portland Trail Blazers", "POR", "West", "Northwest", "blazers");
+            insertTeam(db, R.drawable.uta, "1610612762", "Utah", "Jazz", "Utah Jazz", "UTA", "West", "Northwest", "jazz");
+
+            insertTeam(db, R.drawable.gsw, "1610612744", "Golden State", "Warriors", "Golden State Warriors", "GSW", "West", "Pacific", "warriors");
+            insertTeam(db, R.drawable.lac, "1610612746", "LA", "Clippers", "Los Angeles Clippers", "LAC", "West", "Pacific", "clippers");
+            insertTeam(db, R.drawable.lal, "1610612747", "LA", "Lakers", "Los Angeles Lakers", "LAL", "West", "Pacific", "lakers");
+            insertTeam(db, R.drawable.pho, "1610612756", "Phoenix", "Suns", "Phoenix Suns", "PHX", "West", "Pacific", "suns");
+            insertTeam(db, R.drawable.sac, "1610612758", "Sacramento", "Kings", "Sacramento Kings", "SAC", "West", "Pacific", "kings");
+
         }
         if (oldVersion < 2) {
-            db.execSQL("ALTER TABLE PET ADD COLUMN FAVORITE NUMERIC");
+            db.execSQL("ALTER TABLE TEAMS ADD COLUMN FAVORITE NUMERIC");
         }
     }
 
@@ -73,19 +103,32 @@ public class TeamsDatabaseHelper extends SQLiteOpenHelper {
      * rating Campo para el número de likes
      * resourceId campo identificador numerico de la imagen
      */
-    private void insertPet(SQLiteDatabase db,
-                           int resourceId,
-                           String name,
-                           int rating) {
+
+    private void insertTeam(SQLiteDatabase db,
+                           int image_logo,
+                           String teamId,
+                           String city,
+                           String nickname,
+                           String fullname,
+                           String tricode,
+                           String confname,
+                           String divname,
+                           String urlname) {
         /*Objeto que nos va a permitir indicar que valores queremos
         insertar en la BD*/
-        ContentValues petValues = new ContentValues();
+        ContentValues teamValues = new ContentValues();
         /*
          * creamos cada uno de los campos de la fila a insertar
          */
-        petValues.put("IMAGE_RESOURCE_ID", resourceId);
-        petValues.put("NAME", name);
-        petValues.put("RATING", rating);
-        db.insert("PET", null, petValues);
+        teamValues.put("IMAGE_LOGO", image_logo);
+        teamValues.put("TEAM_ID", teamId);
+        teamValues.put("CITY", city);
+        teamValues.put("NICKNAME", nickname);
+        teamValues.put("FULLNAME", fullname);
+        teamValues.put("TRICODE", tricode);
+        teamValues.put("CONFNAME", confname);
+        teamValues.put("DIVNAME", divname);
+        teamValues.put("URLNAME", urlname);
+        db.insert("TEAMS", null, teamValues);
     }
 }
